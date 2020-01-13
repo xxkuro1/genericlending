07013e844f9cdd1c4820380a0be7e748aae12dc2
skeltonmod <abgaoe@gmail.com>
2020-01-13 15:58:39 +0800

    AJAX

--- html/initialize.php
+++ html/initialize.php
@@ -4,6 +4,12 @@ $db_password = "";
 $db_host = "localhost";
 $db_name = "db_lending";
 
+/*
+CREATE TABLE `db_lending`.`tbl_transaction` ( `id` INT(7) NOT NULL AUTO_INCREMENT , `fname` VARCHAR(255) NOT NULL , `lname` VARCHAR(255) NOT NULL , `address` VARCHAR NOT NULL , `amount` INT(15) NOT NULL , `loan_interest` INT(3) NOT NULL , `top` INT(3) NOT NULL COMMENT 'with interest' , `tp` INT(3) NOT NULL COMMENT 'with interest' , `mp` INT(3) NOT NULL COMMENT 'with interest' , `dd` DATE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+*/
+
+
+
 // open a connection to the driver
 $conn = new mysqli($db_host,$db_username,$db_password);
 
