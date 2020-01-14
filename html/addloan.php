9de9649460b97562e70ebb14a5137f3ea9c38c0c
skeltonmod <abgaoe@gmail.com>
2020-01-14 20:22:46 +0800

    minor changes

--- html/addloan.php
+++ html/addloan.php
@@ -1,5 +1,14 @@
 <?php
 require_once "initialize.php";
+$firstname = $conn->real_escape_string($_POST['fname']);
+$lastname = $conn->real_escape_string($_POST['lname']);
+$address = $conn->real_escape_string($_POST['address']);
+$loaninterest = $_POST['loaninterest'];
+$amount = $_POST['amount'];
+$termofpayment = $_POST['top'];
+$totalpaid = $_POST['tp'];
+$monthlypaid = $_POST['mp'];
+$duedate = $_POST['dd'];
 
 
 ?>
