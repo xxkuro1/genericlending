956fad1b529d5708207532dd590f05ccf1532e58
skeltonmod <abgaoe@gmail.com>
2020-01-14 21:58:56 +0800

    Basic Back End operations

--- html/addloan.php
+++ html/addloan.php
@@ -1,14 +1,37 @@
 <?php
-require_once "initialize.php";
-$firstname = $conn->real_escape_string($_POST['fname']);
-$lastname = $conn->real_escape_string($_POST['lname']);
-$address = $conn->real_escape_string($_POST['address']);
+$db_username = "root";
+$db_password = "mysql";
+$db_host = "localhost";
+$db_name = "db_lending";
+
+$firstname = $_POST['fname'];
+$lastname = $_POST['lname'];
+$address = $_POST['address'];
 $loaninterest = $_POST['loaninterest'];
 $amount = $_POST['amount'];
-$termofpayment = $_POST['top'];
-$totalpaid = $_POST['tp'];
-$monthlypaid = $_POST['mp'];
-$duedate = $_POST['dd'];
+$termofpayment = $_POST['termofpayment'];
+$totalpaid = $_POST['totalpaid'];
+$monthlypaid = $_POST['monthlypaid'];
+$duedate = $_POST['duedate'];
+
+$conn = new mysqli($db_host,$db_username,$db_password,$db_name);
+
+if(isset($_POST['key'])){
+    //insert operations
+    if($_POST['key'] == 'insert'){
+        $statement = "INSERT INTO tbl_customer(fname,lname,address,loaninterest,amount,top,tp,mp,dd) 
+        VALUES ('$firstname','$lastname','$address','$loaninterest','$amount','$termofpayment','$totalpaid','$monthlypaid','$duedate')";
+
+        if($conn->query($statement) === TRUE){
+            echo "Insert Successful";
+        }else{
+            echo $conn->error;
+
+        }
+
+    }
+
+}
 
 
 ?>
