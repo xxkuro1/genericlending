be1a8475757cef68f9000f838838d3740342299c
skeltonmod <abgaoe@gmail.com>
2020-01-18 19:32:26 +0800

    almost there

--- php/view.php
+++ php/view.php
@@ -15,12 +15,12 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td id=\"transaction_id_$row[$tid]\">$row[$tid]</td>";
-        echo "<td id=\"transaction_fname_$row[$fname]\">$row[$fname]</td>";
-        echo "<td>$row[$lname]</td>";
-        echo "<td>$row[$address]</td>";
+        echo "<td id=\"transaction_fname_$row[$tid]\">$row[$fname]</td>";
+        echo "<td id=\"transaction_lname_$row[$tid]\">$row[$lname]</td>";
+        echo "<td id=\"transaction_address_$row[$tid]\">$row[$address]</td>";
         echo "<td>
-                <button type=\"button\"  class=\"button button-primary actionbutton\" onclick=\"viewData($row[$tid],'view')\" 
-                id=\"btnView\">View</button><button type=\"button\"  class=\"button button-primary actionbutton\" onclick=\"deleteData($row[$tid])\" 
+                <button type=\"button\" data-target=\"modal1\"  class=\"waves-effect modal-trigger waves-light btn actionbutton\" onclick=\"viewData($row[$tid],'view')\" 
+                id=\"btnView\">View</button><button type=\"button\"  class=\"waves-effect waves-light btn actionbutton\" onclick=\"deleteData($row[$tid])\" 
                 id=\"btnDelete\">Delete</button>
                 
                 </td>";
