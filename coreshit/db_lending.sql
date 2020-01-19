a2a687ce935541b64ca3b7a8b87e4348810a20b1
skeltonmod <abgaoe@gmail.com>
2020-01-19 23:45:57 +0800

    some fix

--- coreshit/db_lending.sql
+++ coreshit/db_lending.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 19, 2020 at 12:07 PM
+-- Generation Time: Jan 19, 2020 at 03:45 PM
 -- Server version: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -27,19 +27,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_history` (
-  `pid` int(7) NOT NULL,
+  `hid` int(7) NOT NULL,
   `currentamount` double(7,3) NOT NULL,
   `currentbalance` double(7,3) NOT NULL,
-  `penalty` int(7) NOT NULL,
+  `penalty` double(7,3) NOT NULL,
+  `dop` date NOT NULL COMMENT 'date of payment',
   `tid` int(7) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_history`
 --
 
-INSERT INTO `tbl_history` (`pid`, `currentamount`, `currentbalance`, `penalty`, `tid`) VALUES
-(1, 0.000, 2200.000, 183, 1);
+INSERT INTO `tbl_history` (`hid`, `currentamount`, `currentbalance`, `penalty`, `dop`, `tid`) VALUES
+(1, 2000.000, 2200.000, 0.000, '0000-00-00', 1),
+(2, 3000.000, 3300.000, 300.000, '2020-01-19', 1),
+(3, 5000.000, 5500.000, 0.000, '0000-00-00', 2),
+(4, 1500.000, 1650.000, 150.000, '2020-01-19', 2);
 
 -- --------------------------------------------------------
 
@@ -58,14 +62,15 @@ CREATE TABLE IF NOT EXISTS `tbl_transaction` (
   `tp` double(7,3) NOT NULL,
   `mp` double(7,3) NOT NULL,
   `dd` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_transaction`
 --
 
 INSERT INTO `tbl_transaction` (`tid`, `fname`, `lname`, `address`, `loaninterest`, `amount`, `top`, `tp`, `mp`, `dd`) VALUES
-(1, 'Elijah', 'Abgao', 'Molugan', 10, 2000, 1, 2200.000, 183.333, '2022-08-18');
+(1, 'Elijah', 'Abgao', 'Montana', 10, 2000, 1, 3300.000, 275.000, '2020-08-18'),
+(2, 'James', 'Apal', 'Ila kyasu dapit', 10, 5000, 1, 1650.000, 137.500, '2020-08-18');
 
 --
 -- Indexes for dumped tables
@@ -75,8 +80,7 @@ INSERT INTO `tbl_transaction` (`tid`, `fname`, `lname`, `address`, `loaninterest
 -- Indexes for table `tbl_history`
 --
 ALTER TABLE `tbl_history`
-  ADD PRIMARY KEY (`pid`),
-  ADD KEY `tid` (`tid`);
+  ADD PRIMARY KEY (`hid`);
 
 --
 -- Indexes for table `tbl_transaction`
@@ -92,29 +96,12 @@ ALTER TABLE `tbl_transaction`
 -- AUTO_INCREMENT for table `tbl_history`
 --
 ALTER TABLE `tbl_history`
-  MODIFY `pid` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `hid` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tbl_transaction`
 --
 ALTER TABLE `tbl_transaction`
-  MODIFY `tid` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tbl_history`
---
-ALTER TABLE `tbl_history`
-  ADD CONSTRAINT `tbl_history_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `tbl_transaction` (`tid`),
-  ADD CONSTRAINT `tbl_history_ibfk_2` FOREIGN KEY (`tid`) REFERENCES `tbl_transaction` (`tid`);
-
---
--- Constraints for table `tbl_transaction`
---
-ALTER TABLE `tbl_transaction`
-  ADD CONSTRAINT `tbl_transaction_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `tbl_history` (`tid`);
-
+  MODIFY `tid` int(7) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
