c878022bd71d33a97de700f79ec43633272badfc
Kuro <phantommind1234@gmail.com>
2020-01-20 13:34:26 +0800

    bug fix

--- php/addloan.php
+++ php/addloan.php
@@ -19,8 +19,8 @@ $conn = new mysqli($db_host,$db_username,$db_password,$db_name);
 if(isset($_POST['key'])){
     //insert operations
     if($_POST['key'] == 'insert'){
-        $statement = "INSERT INTO tbl_transaction(fname,lname,address,loaninterest,amount,top,tp,mp,dd) 
-        VALUES ('$firstname','$lastname','$address','$loaninterest','$amount','$termofpayment','$totalpaid','$monthlypaid','$duedate')";
+        $statement = "INSERT INTO tbl_transaction(tid, fname,lname,address,loaninterest,amount,top,tp,mp,dd) 
+        VALUES ('$tid','$firstname','$lastname','$address','$loaninterest','$amount','$termofpayment','$totalpaid','$monthlypaid','$duedate')";
         $statement2 = "INSERT INTO tbl_history(currentamount,currentbalance,penalty,dop,tid) VALUES ('$amount','$totalpaid','','','$tid')";
         if($conn->query($statement) === TRUE && $conn->query($statement2) === TRUE ){
             echo "Insert Successful";
